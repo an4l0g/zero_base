50238bbb438b5016622bfde7014e1028c6a7baa9
Ryan Drumond <ryandrumond@ioasys.com.br>
2023-06-05 02:25:18 -0300

    feat: adding pm2 structure

---
+++ pm2.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd "C:\Users\an4log\Desktop\zero_base\server-data\pm2"
+call zero_hud.bat
+
+pause
---
+++ pm2/zero_hud.bat
@@ -0,0 +1,17 @@
+:: zero_hud
+set nui_name="zero_hud"
+set nui_path="C:\Users\an4log\Desktop\zero_base\server-data\resources\[zero]\zero_hud\nui"
+set nui_path_build="C:\Users\an4log\Desktop\zero_base\server-data\resources\[zero]\zero_hud\nui\dist"
+set nui_port=8501
+
+call pm2 delete %nui_name% --silent
+
+:: buildando
+cd %nui_path%
+call yarn build
+
+echo []
+
+:: iniciando processo
+call pm2 serve %nui_path_build% -s --name %nui_path_build% --port %nui_port%
+pause
