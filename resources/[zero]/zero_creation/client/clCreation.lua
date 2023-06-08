99209ecdf2f0447898bf1b23595d0dbfe37f2e4b
Ryan Drumond <ryandrumond@ioasys.com.br>
2023-06-08 04:03:38 -0300

    fix: removing prints

--- resources/[zero]/zero_creation/client/clCreation.lua
+++ resources/[zero]/zero_creation/client/clCreation.lua
@@ -270,11 +270,11 @@ local currentCharacter = {
     makeupOpacity = 0.99,
 }
 
-local currentGender = 'masculino'
+local atualGender = 'masculino'
 
 local setGender = function(gender) 
-    -- currentGender = gender
     local ped = PlayerPedId()
+
     local model = 'mp_m_freemode_01'
     if (gender == 'female') then model = 'mp_f_freemode_01' end
 
@@ -302,9 +302,9 @@ local setGender = function(gender)
     resetClothes()
 
     if (gender == 'male') then
-        SetPedHeadBlendData(ped, 0, 21, 0, 21, 0, 0, 0.8, 0.8, 0.0, false)
+        SetPedHeadBlendData(ped, 21, 0, 0, 21, 0, 0, 0.8, 0.8, 0.0, false)
     else
-        SetPedHeadBlendData(ped, 0, 21, 0, 21, 0, 0, 0.2, 0.2, 0.0, false)
+        SetPedHeadBlendData(ped, 21, 0, 0, 21, 0, 0, 0.2, 0.2, 0.0, false)
     end
 end
 
@@ -389,8 +389,7 @@ RegisterNuiCallback('changeCharacter', function(data)
 
     -- GENDER
     currentCharacter.gender = data.gender
-    if (currentGender ~= data.gender) then
-        currentGender = data.gender
+    if (atualGender ~= data.gender) then
         if (data.gender == 'masculino') then setGender('male'); else setGender('female'); end;
     end
 
--- resources/[zero]/zero_creation/nui/src/App.jsx
+++ resources/[zero]/zero_creation/nui/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   const nuiMessage = useCallback(
     (event) => {
       const { action } = event.data;
-      console.log("TESTE", action);
 
       if (action === "open") setCreation(true);
     },
