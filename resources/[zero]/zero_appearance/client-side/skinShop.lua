e313dee1f8c69f9547657d712dc28a0007c9cc2a
bluenzzz <danielbraga472@gmail.com>
2023-06-19 17:39:49 -0300

    fix skinshop and barbershop

--- resources/[zero]/zero_appearance/client-side/skinShop.lua
+++ resources/[zero]/zero_appearance/client-side/skinShop.lua
@@ -116,6 +116,24 @@ local updateSkinCompras = function()
     return value
 end
 
+local getDrawables = function()
+    local ped = PlayerPedId()
+    local pedDrawables = {
+        torso = { GetNumberOfPedDrawableVariations(ped, 3), '3' },
+        legs = { GetNumberOfPedDrawableVariations(ped, 4), '4' },
+        hands = { GetNumberOfPedDrawableVariations(ped, 5), '5' },
+        fool = { GetNumberOfPedDrawableVariations(ped, 6), '6' },
+        accessories = { GetNumberOfPedDrawableVariations(ped, 8), '8' },
+        acessories2 = { GetNumberOfPedDrawableVariations(ped, 9), '9' },
+        decals = { GetNumberOfPedDrawableVariations(ped, 10), '10' },
+        torso2 = { GetNumberOfPedDrawableVariations(ped, 11), '11' },
+        helmet = { GetNumberOfPedPropDrawableVariations(ped, 0), 'p0' },
+        glasses = { GetNumberOfPedPropDrawableVariations(ped, 1), 'p1' },
+        ear = { GetNumberOfPedPropDrawableVariations(ped, 2), 'p2' }
+    }
+    return pedDrawables
+end
+
 local getSkinShopDrawables = function(part)
     local ped = PlayerPedId()
     local isProp, index = parsePart(part)
@@ -212,13 +230,13 @@ local openNuiShop = function(config, _oldCustom)
     local ped = PlayerPedId()
     SetNuiFocus(true, true)
 
-    local pedModel = GetEntityModel(ped)
     local sex = ''
-    local prefix = ''
+    local pedModel = GetEntityModel(ped)
+    
     if (pedModel == GetHashKey('mp_m_freemode_01')) then
-        sex = 'Male'; prefix = 'M';
+        sex = 'male'
     elseif (pedModel == GetHashKey('mp_f_freemode_01')) then
-        sex = 'Female'; prefix = 'F';
+        sex = 'female'
     else
         local customPeds = config['customPeds']
         if (customPeds) then
@@ -226,16 +244,14 @@ local openNuiShop = function(config, _oldCustom)
             if (allowed) then
                 if type(allowed) == 'string' then
                     sex = allowed
-                    prefix = allowed:sub(1,1)
                 else
                     sex = 'Male'
-                    prefix = 'M'
                 end
             end
         end
     end
 
-    drawVariations = getSkinShopDrawables(categoryVariation)
+    drawVariations = getDrawables()
     drawProps = getSkinShopDrawables(categoryProp)
     
     SendNUIMessage({ 
@@ -243,13 +259,8 @@ local openNuiShop = function(config, _oldCustom)
         data = {
             type = config['shopType'], 
             config = config['shopConfig'], 
-            oldCustom = _oldCustom, 
             sex = sex, 
-            prefix = prefix, 
-            drawVariations = drawVariations, 
-            categoryVariation = categoryVariation, 
-            drawProps = drawProps, 
-            categoryProp = categoryProp 
+            drawVariations = drawVariations
         }
     })
 
