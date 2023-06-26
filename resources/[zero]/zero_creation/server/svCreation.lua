95db9cf881955f99be656e3aed358b06867d6120
bluenzzz <danielbraga472@gmail.com>
2023-06-26 01:49:01 -0300

    fix ped custom

--- resources/[zero]/zero_creation/server/svCreation.lua
+++ resources/[zero]/zero_creation/server/svCreation.lua
@@ -44,10 +44,7 @@ end
 
 local userLogin = {}
 
-AddEventHandler('vRP:playerSpawn', function(user_id, source)
-    local getCharacter = zero.query('zero_character/getCharacter', { user_id = user_id })[1]
-    if (getCharacter) then Player(source).state.pedCustom = json.decode(getCharacter.user_character); end;
-    
+AddEventHandler('vRP:playerSpawn', function(user_id, source)    
     local bloodGroup = generalConfig.bloodGroup
     zero.execute('zero_character/createUser', { user_id = user_id, controller = 0, user_character = '{}', rh = bloodGroup[math.random(#bloodGroup)] })
 
