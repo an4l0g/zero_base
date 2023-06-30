c2448e1ccd1dc48f36b6898cef6540174f251c2d
bluenzzz <danielbraga472@gmail.com>
2023-06-30 15:54:02 -0300

    fix again

--- resources/[zero]/zero_appearance/fxmanifest.lua
+++ resources/[zero]/zero_appearance/fxmanifest.lua
@@ -5,11 +5,8 @@ author 'bluenzzz#0001 & an4log#0001'
 description 'Zero Appearance'
 version '0.1'
 
-<<<<<<< HEAD
 ui_page 'http://localhost:5173/'
 
-=======
->>>>>>> ec0b34ce7ab0cb5983940c0d0ed4cfa96fb7aa45
 client_scripts { 'client-side/main.lua', 'client-side/*.lua' }
 server_scripts { 'server-side/main.lua', 'server-side/*.lua' }
 shared_scripts { '@zero/lib/utils.lua', 'cfg/main.lua', 'cfg/*.lua' }              
