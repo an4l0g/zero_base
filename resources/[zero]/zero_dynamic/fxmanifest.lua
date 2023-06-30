d35b080ff12864cfb3209013ac85faca9eeaa664
bluenzzz <danielbraga472@gmail.com>
2023-06-30 15:57:59 -0300

    fix

--- resources/[zero]/zero_dynamic/fxmanifest.lua
+++ resources/[zero]/zero_dynamic/fxmanifest.lua
@@ -1,13 +1,19 @@
-fx_version 'bodacious'
-game 'gta5'
-
-author 'bluenzzz & an4log'
-description 'Zero Dynamic'
-version '0.1'
+lua54 'yes'
+fx_version "bodacious"
+game "gta5"
 
 ui_page "http://189.0.88.222:8504"
 -- ui_page "http://localhost:5173"
 
-client_script { 'client/*.lua' }
-server_scripts { 'server/*.lua' }
-shared_scripts { '@zero/lib/utils.lua', 'config.lua' }
+client_script {
+    "client/*.lua"
+}
+
+server_scripts {
+    "server/*.lua"
+}
+
+shared_scripts {
+    "@zero/lib/utils.lua",
+    "config.lua"
+}
