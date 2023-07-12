fdbc0de7376f0dde39075e4bfbc0a6dae207a337
VPS <cadastro@ryandrumond.com>
2023-07-12 16:25:57 -0300

    manifest

--- resources/[zero]/zero_hospital/fxmanifest.lua
+++ resources/[zero]/zero_hospital/fxmanifest.lua
@@ -3,8 +3,8 @@ lua54 'yes'
 fx_version "bodacious"
 game "gta5"
 
---ui_page "http://localhost:8502"
-ui_page "http://localhost:5173"
+ui_page "http://189.0.88.222:8507"
+-- ui_page "http://localhost:5173"
 
 client_script {
     "client/*.lua"
--- resources/[zero]/zero_inventory/fxmanifest.lua
+++ resources/[zero]/zero_inventory/fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version "bodacious"
 game "gta5"
 
-ui_page "http://localhost:8502"
--- ui_page "http://189.0.88.222:8502"
+-- ui_page "http://localhost:8502"
+ui_page "http://189.0.88.222:8502"
 
 client_script {
     "crosscript/client.lua",
