5130212f30fe7ed49eec8b050015c826732e4939
bluenzzz <danielbraga472@gmail.com>
2023-07-19 16:00:50 -0300

    fix

--- resources/[zero]/zero_appearance/client-side/clSkin.lua
+++ resources/[zero]/zero_appearance/client-side/clSkin.lua
@@ -7,21 +7,20 @@ local generalConfig =  config.general
 local getDrawables = function()
     local ped = PlayerPedId()
     local pedDrawables = {
-        { amount = GetNumberOfPedDrawableVariations(ped, 1), type = '1' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 3), type = '3' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 4), type = '4' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 5), type = '5' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 6), type = '6' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 7), type = '7' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 8), type = '8' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 9), type = '9' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 10), type = '10' },
-        { amount = GetNumberOfPedDrawableVariations(ped, 11), type = '11' },
-        { amount = GetNumberOfPedPropDrawableVariations(ped, 0), type = 'p0' },
-        { amount = GetNumberOfPedPropDrawableVariations(ped, 1), type = 'p1' },
-        { amount = GetNumberOfPedPropDrawableVariations(ped, 2), type = 'p2' },
-        { amount = GetNumberOfPedPropDrawableVariations(ped, 6), type = 'p6' },
-        { amount = GetNumberOfPedPropDrawableVariations(ped, 7), type = 'p7' }
+        { amount = GetNumberOfPedDrawableVariations(ped, 1), type = '1', model = GetPedDrawableVariation(ped, 1), var = GetPedTextureVariation(ped, 1)  },
+        { amount = GetNumberOfPedDrawableVariations(ped, 3), type = '3', model = GetPedDrawableVariation(ped, 3), var = GetPedTextureVariation(ped, 3) },
+        { amount = GetNumberOfPedDrawableVariations(ped, 4), type = '4', model = GetPedDrawableVariation(ped, 4), var = GetPedTextureVariation(ped, 4) },
+        { amount = GetNumberOfPedDrawableVariations(ped, 5), type = '5', model = GetPedDrawableVariation(ped, 5), var = GetPedTextureVariation(ped, 5) },
+        { amount = GetNumberOfPedDrawableVariations(ped, 6), type = '6', model = GetPedDrawableVariation(ped, 6), var = GetPedTextureVariation(ped, 6) },
+        { amount = GetNumberOfPedDrawableVariations(ped, 7), type = '7', model = GetPedDrawableVariation(ped, 7), var = GetPedTextureVariation(ped, 7) },
+        { amount = GetNumberOfPedDrawableVariations(ped, 8), type = '8', model = GetPedDrawableVariation(ped, 8), var = GetPedTextureVariation(ped, 8) },
+        { amount = GetNumberOfPedDrawableVariations(ped, 9), type = '9', model = GetPedDrawableVariation(ped, 9), var = GetPedTextureVariation(ped, 9) },
+        { amount = GetNumberOfPedDrawableVariations(ped, 11), type = '11', model = GetPedDrawableVariation(ped, 11), var = GetPedTextureVariation(ped, 11) },
+        { amount = GetNumberOfPedPropDrawableVariations(ped, 0), type = 'p0', model = GetPedPropIndex(ped, 0), var = GetPedPropTextureIndex(ped, 0) },
+        { amount = GetNumberOfPedPropDrawableVariations(ped, 1), type = 'p1', model = GetPedPropIndex(ped, 1), var = GetPedPropTextureIndex(ped, 1) },
+        { amount = GetNumberOfPedPropDrawableVariations(ped, 2), type = 'p2', model = GetPedPropIndex(ped, 2), var = GetPedPropTextureIndex(ped, 2) },
+        { amount = GetNumberOfPedPropDrawableVariations(ped, 6), type = 'p6', model = GetPedPropIndex(ped, 6), var = GetPedPropTextureIndex(ped, 6) },
+        { amount = GetNumberOfPedPropDrawableVariations(ped, 7), type = 'p7', model = GetPedPropIndex(ped, 7), var = GetPedPropTextureIndex(ped, 7) }
     }
     return pedDrawables
 end
@@ -116,3 +115,17 @@ openSkinShop = function(locs)
     if (not DoesCamExist(tempCam)) then createCam('body'); end;
     FreezeEntityPosition(ped, true)
 end
+
+RegisterNUICallback('getSkinVariations', function(data, cb)
+    local ped = PlayerPedId()
+
+    local isProp, index = parsePart(data.type)
+    if (isProp) then max = GetNumberOfPedPropTextureVariations(ped, index, data.model);
+    else max = GetNumberOfPedTextureVariations(ped, 11, data.model);
+    end
+
+    SendNUIMessage({
+        action = 'setSkinVariantions',
+        data = max
+    })
+end)
