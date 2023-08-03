418e8fba9936efb145b3b8172b630304acc24494
bluenzzz <danielbraga472@gmail.com>
2023-08-02 21:24:00 -0300

    fix

--- resources/[zero]/zero_creation/cfg/cfgCreation.lua
+++ resources/[zero]/zero_creation/cfg/cfgCreation.lua
@@ -1,7 +1,6 @@
 configCreator = {}
 
 configCreator.general = {
-    ['maxHealth'] = 400,
     ['spawnCreator'] = vector4(-2043.7, -1031.62, 11.99, 70.866142272949),
     ['spawnAfterCreator'] = vector4(-1037.74, -2737.8, 20.17, 331.65353393555),
     ['blacklistNames'] = { ['sexo'] = true, ['anau'] = true, ['anal'] = true, ['oral'] = true, ['leite'] = true, ['jacinto'] = true, ['macaco'] = true, ['preto'] = true, ['pretinho'] = true, ['gorila'] = true, ['pretudo'] = true, ['pinto'] = true, ['rola'] = true, ['pauzudo'] = true, ['pintudo'] = true, ['turbando'] = true, ['masturbando'] = true, ['masturbar'] = true, ['pauzao'] = true, ['lgbt'] = true, ['lgbtqia+'] = true, ['lgbtqia'] = true, ['gay'] = true, ['viado'] = true, ['viadao'] = true, ['puta'] = true, ['puto'] = true },
