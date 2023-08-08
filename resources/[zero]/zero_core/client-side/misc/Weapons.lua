163c708a8ae761d5b6623f7bc8cd31de9cede9d9
bluenzzz <danielbraga472@gmail.com>
2023-08-08 19:24:46 -0300

    fix switch weapon

--- resources/[zero]/zero_core/client-side/misc/Weapons.lua
+++ resources/[zero]/zero_core/client-side/misc/Weapons.lua
@@ -1,131 +1,131 @@
 local Weapons = {
 	-- MELEE
-    [GetHashKey('WEAPON_DAGGER')] = true,
-    [GetHashKey('WEAPON_BAT')] = true,
-    [GetHashKey('WEAPON_BOTTLE')] = true,
-    [GetHashKey('WEAPON_CROWBAR')] = true,
-    [GetHashKey('WEAPON_FLASHLIGHT')] = true,
-    [GetHashKey('WEAPON_GOLFCLUB')] = true,
-    [GetHashKey('WEAPON_HAMMER')] = true,
-    [GetHashKey('WEAPON_HATCHET')] = true,
-    [GetHashKey('WEAPON_KNUCKLE')] = true,
-    [GetHashKey('WEAPON_KNIFE')] = true,
-    [GetHashKey('WEAPON_MACHETE')] = true,
-    [GetHashKey('WEAPON_SWITCHBLADE')] = true,
-    [GetHashKey('WEAPON_NIGHTSTICK')] = true,
-    [GetHashKey('WEAPON_WRENCH')] = true,
-    [GetHashKey('WEAPON_BATTLEAXE')] = true,
-    [GetHashKey('WEAPON_POOLCUE')] = true,
-    [GetHashKey('WEAPON_STONE_HATCHET')] = true,
-    [GetHashKey('WEAPON_CANDYCANE')] = true,
+    'WEAPON_DAGGER',
+    'WEAPON_BAT',
+    'WEAPON_BOTTLE',
+    'WEAPON_CROWBAR',
+    'WEAPON_FLASHLIGHT',
+    'WEAPON_GOLFCLUB',
+    'WEAPON_HAMMER',
+    'WEAPON_HATCHET',
+    'WEAPON_KNUCKLE',
+    'WEAPON_KNIFE',
+    'WEAPON_MACHETE',
+    'WEAPON_SWITCHBLADE',
+    'WEAPON_NIGHTSTICK',
+    'WEAPON_WRENCH',
+    'WEAPON_BATTLEAXE',
+    'WEAPON_POOLCUE',
+    'WEAPON_STONE_HATCHET',
+    'WEAPON_CANDYCANE',
 
     -- HANDGUNS
-    [GetHashKey('WEAPON_PISTOL')] = true,
-    [GetHashKey('WEAPON_PISTOL_MK2')] = true,
-    [GetHashKey('WEAPON_COMBATPISTOL')] = true,
-    [GetHashKey('WEAPON_APPISTOL')] = true,
-    [GetHashKey('WEAPON_STUNGUN')] = true,
-    [GetHashKey('WEAPON_PISTOL50')] = true,
-    [GetHashKey('WEAPON_SNSPISTOL')] = true,
-    [GetHashKey('WEAPON_SNSPISTOL_MK2')] = true,
-    [GetHashKey('WEAPON_HEAVYPISTOL')] = true,
-    [GetHashKey('WEAPON_VINTAGEPISTOL')] = true,
-    [GetHashKey('WEAPON_FLAREGUN')] = true,
-    [GetHashKey('WEAPON_MARKSMANPISTOL')] = true,
-    [GetHashKey('WEAPON_REVOLVER')] = true,
-    [GetHashKey('WEAPON_REVOLVER_MK2')] = true,
-    [GetHashKey('WEAPON_DOUBLEACTION')] = true,
-    [GetHashKey('WEAPON_RAYPISTOL')] = true,
-    [GetHashKey('WEAPON_CERAMICPISTOL')] = true,
-    [GetHashKey('WEAPON_NAVYREVOLVER')] = true,
-    [GetHashKey('WEAPON_GADGETPISTOL')] = true,
-    [GetHashKey('WEAPON_STUNGUN_MP')] = true,
-    [GetHashKey('WEAPON_PISTOLXM3')] = true,
+    'WEAPON_PISTOL',
+    'WEAPON_PISTOL_MK2',
+    'WEAPON_COMBATPISTOL',
+    'WEAPON_APPISTOL',
+    'WEAPON_STUNGUN',
+    'WEAPON_PISTOL50',
+    'WEAPON_SNSPISTOL',
+    'WEAPON_SNSPISTOL_MK2',
+    'WEAPON_HEAVYPISTOL',
+    'WEAPON_VINTAGEPISTOL',
+    'WEAPON_FLAREGUN',
+    'WEAPON_MARKSMANPISTOL',
+    'WEAPON_REVOLVER',
+    'WEAPON_REVOLVER_MK2',
+    'WEAPON_DOUBLEACTION',
+    'WEAPON_RAYPISTOL',
+    'WEAPON_CERAMICPISTOL',
+    'WEAPON_NAVYREVOLVER',
+    'WEAPON_GADGETPISTOL',
+    'WEAPON_STUNGUN_MP',
+    'WEAPON_PISTOLXM3',
 
     -- SUBMACHINE GUNS
-    [GetHashKey('WEAPON_MICROSMG')] = true,
-    [GetHashKey('WEAPON_SMG')] = true,
-    [GetHashKey('WEAPON_SMG_MK2')] = true,
-    [GetHashKey('WEAPON_ASSAULTSMG')] = true,
-    [GetHashKey('WEAPON_COMBATPDW')] = true,
-    [GetHashKey('WEAPON_MACHINEPISTOL')] = true,
-    [GetHashKey('WEAPON_MINISMG')] = true,
-    [GetHashKey('WEAPON_RAYCARBINE')] = true,
-    [GetHashKey('WEAPON_TECPISTOL')] = true,
+    'WEAPON_MICROSMG',
+    'WEAPON_SMG',
+    'WEAPON_SMG_MK2',
+    'WEAPON_ASSAULTSMG',
+    'WEAPON_COMBATPDW',
+    'WEAPON_MACHINEPISTOL',
+    'WEAPON_MINISMG',
+    'WEAPON_RAYCARBINE',
+    'WEAPON_TECPISTOL',
 
     -- SHOTGUNS
-    [GetHashKey('WEAPON_PUMPSHOTGUN')] = true,
-    [GetHashKey('WEAPON_PUMPSHOTGUN_MK2')] = true,
-    [GetHashKey('WEAPON_SAWNOFFSHOTGUN')] = true,
-    [GetHashKey('WEAPON_ASSAULTSHOTGUN')] = true,
-    [GetHashKey('WEAPON_BULLPUPSHOTGUN')] = true,
-    [GetHashKey('WEAPON_MUSKET')] = true,
-    [GetHashKey('WEAPON_HEAVYSHOTGUN')] = true,
-    [GetHashKey('WEAPON_DBSHOTGUN')] = true,
-    [GetHashKey('WEAPON_AUTOSHOTGUN')] = true,
-    [GetHashKey('WEAPON_COMBATSHOTGUN')] = true,
+    'WEAPON_PUMPSHOTGUN',
+    'WEAPON_PUMPSHOTGUN_MK2',
+    'WEAPON_SAWNOFFSHOTGUN',
+    'WEAPON_ASSAULTSHOTGUN',
+    'WEAPON_BULLPUPSHOTGUN',
+    'WEAPON_MUSKET',
+    'WEAPON_HEAVYSHOTGUN',
+    'WEAPON_DBSHOTGUN',
+    'WEAPON_AUTOSHOTGUN',
+    'WEAPON_COMBATSHOTGUN',
 
     -- ASSAULT RIFLE
-    [GetHashKey('WEAPON_ASSAULTRIFLE')] = true,
-    [GetHashKey('WEAPON_ASSAULTRIFLE_MK2')] = true,
-    [GetHashKey('WEAPON_CARBINERIFLE')] = true,
-    [GetHashKey('WEAPON_CARBINERIFLE_MK2')] = true,
-    [GetHashKey('WEAPON_ADVANCEDRIFLE')] = true,
-    [GetHashKey('WEAPON_SPECIALCARBINE')] = true,
-    [GetHashKey('WEAPON_SPECIALCARBINE_MK2')] = true,
-    [GetHashKey('WEAPON_BULLPUPRIFLE')] = true,
-    [GetHashKey('WEAPON_BULLPUPRIFLE_MK2')] = true,
-    [GetHashKey('WEAPON_COMPACTRIFLE')] = true,
-    [GetHashKey('WEAPON_MILITARYRIFLE')] = true,
-    [GetHashKey('WEAPON_HEAVYRIFLE')] = true,
-    [GetHashKey('WEAPON_TACTICALRIFLE')] = true,
+    'WEAPON_ASSAULTRIFLE',
+    'WEAPON_ASSAULTRIFLE_MK2',
+    'WEAPON_CARBINERIFLE',
+    'WEAPON_CARBINERIFLE_MK2',
+    'WEAPON_ADVANCEDRIFLE',
+    'WEAPON_SPECIALCARBINE',
+    'WEAPON_SPECIALCARBINE_MK2',
+    'WEAPON_BULLPUPRIFLE',
+    'WEAPON_BULLPUPRIFLE_MK2',
+    'WEAPON_COMPACTRIFLE',
+    'WEAPON_MILITARYRIFLE',
+    'WEAPON_HEAVYRIFLE',
+    'WEAPON_TACTICALRIFLE',
 
     -- LIGHT MACHINE GUNS
-    [GetHashKey('WEAPON_MG')] = true,
-    [GetHashKey('WEAPON_COMBATMG')] = true,
-    [GetHashKey('WEAPON_COMBATMG_MK2')] = true,
-    [GetHashKey('WEAPON_GUSENBERG')] = true,
+    'WEAPON_MG',
+    'WEAPON_COMBATMG',
+    'WEAPON_COMBATMG_MK2',
+    'WEAPON_GUSENBERG',
 
     -- SNIPER RIFLES
-    [GetHashKey('WEAPON_SNIPERRIFLE')] = true,
-    [GetHashKey('WEAPON_HEAVYSNIPER')] = true,
-    [GetHashKey('WEAPON_HEAVYSNIPER_MK2')] = true,
-    [GetHashKey('WEAPON_MARKSMANRIFLE')] = true,
-    [GetHashKey('WEAPON_MARKSMANRIFLE_MK2')] = true,
-    [GetHashKey('WEAPON_PRECISIONRIFLE')] = true,
+    'WEAPON_SNIPERRIFLE',
+    'WEAPON_HEAVYSNIPER',
+    'WEAPON_HEAVYSNIPER_MK2',
+    'WEAPON_MARKSMANRIFLE',
+    'WEAPON_MARKSMANRIFLE_MK2',
+    'WEAPON_PRECISIONRIFLE',
 
     -- HEAVY WEAPONS
-    [GetHashKey('WEAPON_RPG')] = true,
-    [GetHashKey('WEAPON_GRENADELAUNCHER')] = true,
-    [GetHashKey('WEAPON_GRENADELAUNCHER_SMOKE')] = true,
-    [GetHashKey('WEAPON_MINIGUN')] = true,
-    [GetHashKey('WEAPON_FIREWORK')] = true,
-    [GetHashKey('WEAPON_RAILGUN')] = true,
-    [GetHashKey('WEAPON_HOMINGLAUNCHER')] = true,
-    [GetHashKey('WEAPON_COMPACTLAUNCHER')] = true,
-    [GetHashKey('WEAPON_RAYMINIGUN')] = true,
-    [GetHashKey('WEAPON_EMPLAUNCHER')] = true,
-    [GetHashKey('WEAPON_RAILGUNXM3')] = true,
+    'WEAPON_RPG',
+    'WEAPON_GRENADELAUNCHER',
+    'WEAPON_GRENADELAUNCHER_SMOKE',
+    'WEAPON_MINIGUN',
+    'WEAPON_FIREWORK',
+    'WEAPON_RAILGUN',
+    'WEAPON_HOMINGLAUNCHER',
+    'WEAPON_COMPACTLAUNCHER',
+    'WEAPON_RAYMINIGUN',
+    'WEAPON_EMPLAUNCHER',
+    'WEAPON_RAILGUNXM3',
     
     -- THROWABLES
-    [GetHashKey('WEAPON_GRENADE')] = true,
-    [GetHashKey('WEAPON_BZGAS')] = true,
-    [GetHashKey('WEAPON_MOLOTOV')] = true,
-    [GetHashKey('WEAPON_STICKYBOMB')] = true,
-    [GetHashKey('WEAPON_PROXMINE')] = true,
-    [GetHashKey('WEAPON_SNOWBALL')] = true,
-    [GetHashKey('WEAPON_PIPEBOMB')] = true,
-    [GetHashKey('WEAPON_BALL')] = true,
-    [GetHashKey('WEAPON_SMOKEGRENADE')] = true,
-    [GetHashKey('WEAPON_FLARE')] = true,
-    [GetHashKey('WEAPON_ACIDPACKAGE')] = true,
+    'WEAPON_GRENADE',
+    'WEAPON_BZGAS',
+    'WEAPON_MOLOTOV',
+    'WEAPON_STICKYBOMB',
+    'WEAPON_PROXMINE',
+    'WEAPON_SNOWBALL',
+    'WEAPON_PIPEBOMB',
+    'WEAPON_BALL',
+    'WEAPON_SMOKEGRENADE',
+    'WEAPON_FLARE',
+    'WEAPON_ACIDPACKAGE',
 
     -- MISC
-    [GetHashKey('WEAPON_PETROLCAN')] = true,
-    [GetHashKey('GADGET_PARACHUTE')] = true,
-    [GetHashKey('WEAPON_FIREEXTINGUISHER')] = true,
-    [GetHashKey('WEAPON_HAZARDCAN')] = true,
-    [GetHashKey('WEAPON_FERTILIZERCAN')] = true
+    'WEAPON_PETROLCAN',
+    'GADGET_PARACHUTE',
+    'WEAPON_FIREEXTINGUISHER',
+    'WEAPON_HAZARDCAN',
+    'WEAPON_FERTILIZERCAN'
 }
 
 WeaponsHash = function()
@@ -153,42 +153,48 @@ Citizen.CreateThread(function()
         local ped = PlayerPedId()
         if (GetEntityHealth(ped) > 100) then
             if (not IsPedInAnyVehicle(ped)) then
-                if (Weapons[GetSelectedPedWeapon(ped)]) then
-                    zero.CarregarAnim('reaction@intimidation@1h')
-                    LastWeapon = GetSelectedPedWeapon(ped)
-                    if (not Holster) then
+                for i = 1, #Weapons do
+                    if (not Holster and LastWeapon ~= nil and LastWeapon ~= GetHashKey(Weapons[i]) and GetSelectedPedWeapon(ped) == GetHashKey(Weapons[i])) then
+                        zero.CarregarAnim('reaction@intimidation@1h')
                         TriggerEvent('disableActionsWeapon', true)
                         SetCurrentPedWeapon(ped, GetHashKey('WEAPON_UNARMED'), true)
                         TaskPlayAnim(ped, 'reaction@intimidation@1h', 'intro', 8.0,8.0,-1,48,10,0,0,0)
                         Citizen.Wait(1200)
-                        SetCurrentPedWeapon(ped, LastWeapon, true)
+                        SetCurrentPedWeapon(ped, GetHashKey(Weapons[i]), true)
                         Citizen.Wait(1300)
-					    ClearPedTasks(ped)
+                        ClearPedTasks(ped)
                         TriggerEvent('disableActionsWeapon', false)
                         Holster = true
                     end
-                else
-                    if (Holster) then
+
+                    if (Holster and LastWeapon == GetHashKey(Weapons[i]) and GetSelectedPedWeapon(ped) == GetHashKey('WEAPON_UNARMED')) then
                         TriggerEvent('disableActionsWeapon', true)
                         SetCurrentPedWeapon(ped, LastWeapon, true)
                         TaskPlayAnim(ped, 'reaction@intimidation@1h', 'outro', 8.0,8.0,-1,48,10,0,0,0)
                         Citizen.Wait(1400)
                         SetCurrentPedWeapon(ped, GetHashKey('WEAPON_UNARMED'), true)
                         Citizen.Wait(600)
-					    ClearPedTasks(ped)
+                        ClearPedTasks(ped)
                         LastWeapon = nil
                         TriggerEvent('disableActionsWeapon', false)
                         Holster = false
                     end
-                end
-            end
 
-            -- if (IsPedArmed(ped, 6)) then threadWeapons(); end;
+                    if (LastWeapon ~= GetSelectedPedWeapon(ped)) then
+                        if (GetSelectedPedWeapon(ped) == GetHashKey('WEAPON_UNARMED')) then
+                            Holster = true
+                        else
+                            Holster = false
+                        end
+                    end
+                end
+                LastWeapon = GetSelectedPedWeapon(ped)
+            end
         else
             if (Holster) then Holster = false; end;
             SetCurrentPedWeapon(ped, GetHashKey('WEAPON_UNARMED'), true)
         end
-		Citizen.Wait(100)
+		Citizen.Wait(500)
 	end
 end)
 
