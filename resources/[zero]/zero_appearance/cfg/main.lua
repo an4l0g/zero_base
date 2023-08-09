4f99f0fb0bb71379357830a0adc813b3452fdfb1
bluenzzz <danielbraga472@gmail.com>
2023-08-08 23:55:45 -0300

    optimize zero_appearance

--- resources/[zero]/zero_appearance/cfg/main.lua
+++
@@ -1,7 +0,0 @@
-Tunnel = module('zero', 'lib/Tunnel')
-Proxy = module('zero', 'lib/Proxy')
-zero = Proxy.getInterface('zero')
-
-if (IsDuplicityVersion()) then
-    zeroClient = Tunnel.getInterface('zero')
-end
