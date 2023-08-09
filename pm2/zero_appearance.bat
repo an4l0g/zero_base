b25b0216c990ea35b3d98cecbf54b6bea82f476d
VPS <cadastro@ryandrumond.com>
2023-08-09 00:03:15 -0300

    fixs

---
+++ pm2/zero_appearance.bat
@@ -0,0 +1,16 @@
+:: zero_production
+set nui_name="zero_production"
+set nui_path=%BASE_PATH%\resources\[zero]\zero_appearance\nui
+set nui_path_build=%BASE_PATH%\resources\[zero]\zero_appearance\nui\dist
+set nui_port=8509
+
+call pm2 delete %nui_name% --silent
+
+:: buildando
+cd %nui_path%
+call yarn install
+cd %nui_path%
+call yarn build
+
+:: iniciando processo
+call pm2 serve %nui_path_build% -s --name %nui_name% --port %nui_port%
