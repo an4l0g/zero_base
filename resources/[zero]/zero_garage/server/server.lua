543839faf5ac72bb126d28965ef8c21d142a5f92
bluenzzz <danielbraga472@gmail.com>
2023-08-09 12:18:06 -0300

    create check permission in homes

--- resources/[zero]/zero_garage/server/server.lua
+++ resources/[zero]/zero_garage/server/server.lua
@@ -40,11 +40,21 @@ Citizen.CreateThread(function()
 	end
 end)
 
-srv.checkPermissions = function(perm)
+local permission = {
+    ['perm'] = function(user_id, perm, home)
+        return zero.checkPermissions(user_id, perm)
+    end,
+    ['home'] = function(user_id, perm, home)
+        return exports['zero_homes']:checkHomePermission(user_id, home)
+    end
+}
+
+srv.checkPermissions = function(perm, home)
     local source = source
     local user_id = zero.getUserId(source)
     if (user_id) then
-        return zero.checkPermissions(user_id, perm)
+        local isHome = (home and 'home' or 'perm')
+        return permission[isHome](user_id, perm, home)
     end
 end
 
