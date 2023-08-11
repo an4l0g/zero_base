8c297e9e653465c0f0e2a8a4a872f45ac63fd4d8
bluenzzz <danielbraga472@gmail.com>
2023-08-11 01:04:09 -0300

    fix

---
+++ resources/[zero]/zero_org/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'bluenzzz#0001 & an4log#0001'
+description 'Zero Appearance'
+version '0.1'
+
+ui_page 'http://localhost:5174/'
+
+client_script 'client/*'
+server_script 'server/*'        
+shared_scripts { '@zero/lib/utils.lua', 'main.lua' }
