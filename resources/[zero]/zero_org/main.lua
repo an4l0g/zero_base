092005a35a7fa611e1706331b98a85b2c38715fe
bluenzzz <danielbraga472@gmail.com>
2023-08-11 00:41:29 -0300

    fix

--- resources/[zero]/zero_org/main.lua
+++ resources/[zero]/zero_org/main.lua
@@ -2,9 +2,9 @@ Tunnel = module('zero','lib/Tunnel')
 Proxy = module('zero','lib/Proxy')
 
 if (IsDuplicityVersion()) then
-    vRP = Tunnel.getInterface('zero')
-    vRPClient = Proxy.getInterface('zero')
+    vRP = Proxy.getInterface('zero')
+    vRPClient = Tunnel.getInterface('zero')
 else
-    vRP = Tunnel.getInterface('zero')
-    vRPserver = Proxy.getInterface('zero')
+    vRP = Proxy.getInterface('zero')
+    vRPserver = Tunnel.getInterface('zero')
 end
