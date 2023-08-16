d088c45ef510f02b8410ec2fd31cf170d72ff887
bluenzzz <danielbraga472@gmail.com>
2023-08-16 10:37:05 -0300

    feature npcs

---
+++ resources/[zero]/zero_core/cfg/cfgPed.lua
@@ -0,0 +1,14 @@
+local config = {}
+
+config.peds = {
+    {
+        coord = vector4(2.703297, -1215.099, 26.88721, 269.2914),
+        hash = 'u_m_y_imporage',
+        anim = {
+            'anim@heists@heist_corona@single_team',
+            'single_team_loop_boss'
+        }
+    }
+}
+
+return config
