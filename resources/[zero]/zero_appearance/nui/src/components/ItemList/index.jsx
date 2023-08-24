8c63ec7fd70f1a042cfe35473a68fc1d1a75588b
Ryan Drumond <ryandrumond@ioasys.com.br>
2023-08-24 17:53:39 -0300

    feat: appearance

--- resources/[zero]/zero_appearance/nui/src/components/ItemList/index.jsx
+++ resources/[zero]/zero_appearance/nui/src/components/ItemList/index.jsx
@@ -1,4 +1,11 @@
-import React, { useCallback, useContext, useEffect, useMemo } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import * as S from "../GenericalStyles";
 import Item from "../Item";
 import ResultContext from "../../contexts/ResultContext";
@@ -9,10 +16,31 @@ function ItemList({ shop, limit, indexType, labelType }) {
   const { appearance } = useContext(AppearanceContext);
   const { result } = useContext(ResultContext);
   const { handleSetResult } = useResult();
+  const [currentSelect, setCurrentSelect] = useState(0);
+  const itemRefs = useRef({});
+
+  const scrollTo = useCallback(() => {
+    const item = itemRefs.current[`item${currentSelect}`];
+    if (item) {
+      item.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [currentSelect]);
 
   useEffect(() => {
-    console.log(appearance);
-  }, []);
+    scrollTo();
+    if (!appearance.tattooshop)
+      handleClick(renderItems[currentSelect], currentSelect);
+  }, [currentSelect]);
+
+  useEffect(() => {
+    if (appearance.tattooshop) {
+      setCurrentSelect(0);
+    } else {
+      setCurrentSelect(result.current[labelType].model);
+    }
+  }, [labelType, appearance]);
 
   const renderItems = useMemo(() => {
     if (appearance.tattooshop) {
@@ -23,17 +51,75 @@ function ItemList({ shop, limit, indexType, labelType }) {
     return new Array(limit).fill(0);
   }, [limit]);
 
+  const interactions = {
+    ArrowLeft: () => {
+      setCurrentSelect((old) => {
+        if (old > -1) {
+          return old - 1;
+        }
+        return old;
+      });
+    },
+    ArrowRight: () => {
+      setCurrentSelect((old) => {
+        if (old !== renderItems.length - 1) return old + 1;
+        return old;
+      });
+    },
+    ArrowUp: () => {
+      setCurrentSelect((old) => {
+        if (old - 3 > -1) {
+          return old - 3;
+        }
+        return old;
+      });
+    },
+    ArrowDown: () => {
+      setCurrentSelect((old) => {
+        if (old + 3 !== renderItems.length - 1) return old + 3;
+        return old;
+      });
+    },
+    Enter: useCallback(() => {
+      handleClick(renderItems[currentSelect], currentSelect);
+    }, [renderItems, currentSelect]),
+  };
+
+  const handleKeyDown = useCallback(
+    (event) => {
+      interactions[event.key] && interactions[event.key]();
+    },
+    [interactions]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
   const handleClick = useCallback(
     (item, index) => {
+      setCurrentSelect(index);
       handleSetResult(labelType, {
         model: appearance.tattooshop ? item : index,
         var: 0,
       });
     },
-    [appearance, labelType]
+    [appearance, labelType, setCurrentSelect]
   );
 
   const renderClassName = useCallback(
+    (index, active) => {
+      if (currentSelect === index) return "current " + active;
+      return active;
+    },
+    [currentSelect]
+  );
+
+  const verifyActiveItem = useCallback(
     (index, item = null) => {
       if (appearance.tattooshop) {
         const filter = result.current[indexType].filter(
@@ -51,20 +137,25 @@ function ItemList({ shop, limit, indexType, labelType }) {
     <S.OptionsList>
       {!appearance.tattooshop && (
         <Item
+          ref={(el) => (itemRefs.current[`item${-1}`] = el)}
           key={-1}
           index={-1}
           shop={shop}
-          className={renderClassName(-1, -1)}
+          className={renderClassName(-1, verifyActiveItem(-1, -1))}
           labelType={labelType}
           handleClick={() => handleClick(-1, -1)}
         />
       )}
       {renderItems.map((item, index) => (
         <Item
+          ref={(el) => (itemRefs.current[`item${index}`] = el)}
           key={index}
           index={index}
           shop={shop}
-          className={renderClassName(index, item)}
+          className={renderClassName(
+            index,
+            verifyActiveItem(index, item, currentSelect)
+          )}
           labelType={labelType}
           tattooImage={item.name}
           handleClick={() => handleClick(item, index)}
