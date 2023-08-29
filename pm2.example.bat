00e7c89bff2fb0145d906126c49638301eef945e
VPS <ryandrumond@outlook.com.br>
2023-08-29 20:26:06 -0300

    merge

---
+++ pm2.example.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set BASE_PATH=
+
+cd %BASE_PATH%\pm2
+call zero_hud.bat
+cd %BASE_PATH%\pm2
+call zero_inventory.bat
+cd %BASE_PATH%\pm2
+call zero_creation.bat
+cd %BASE_PATH%\pm2
+call zero_dynamic.bat
+cd %BASE_PATH%\pm2
+call zero_production.bat
+cd %BASE_PATH%\pm2
+call zero_garage.bat
+cd %BASE_PATH%\pm2
+call zero_identity.bat
+
+echo [zero_base] Todas as NUIs foram iniciadas
+pause
